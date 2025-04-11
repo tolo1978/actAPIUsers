4299bbcfe45e54310d3848908d9e69bfed071fd4
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-04-11 09:36:05 +0200

    Maquetado footer

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private endPoint: string = 'https://peticiones.online/api/users';
+  private httpClient = inject(HttpClient);
 
   constructor() { }
 }
