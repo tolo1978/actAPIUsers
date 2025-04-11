14a61572a64edd280da96ad922974a6cedddd2ba
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-04-11 09:00:03 +0200

    Rutas y 404
    Generado rutas y componente 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'home'},
+    {path: "home", component: HomeComponent},
+    {path: 'user/:id', component: UserViewComponent},
+    {path: "**", component: Error404Component}
+];
