059a896dd2dee4a00b464fc5b76ec163794fc3b2
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-04-12 18:13:51 +0200

    UserForm
    Generado formulario de usuario y configurado en routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserViewComponent } from './pages/user-view/user-view.component';
 import { Error404Component } from './pages/error404/error404.component';
+import { UserFormComponent } from './pages/user-form/user-form.component';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: 'home'},
     {path: "home", component: HomeComponent},
     {path: 'user/:id', component: UserViewComponent},
+    {path: 'user/new', component: UserFormComponent},
     {path: "**", component: Error404Component}
 ];
