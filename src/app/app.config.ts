f6a749aa38be679850b86d19443aa03b99eb8f11
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-04-15 15:30:48 +0200

    icons bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http'; // hay que importarlo para poder hacer peticiones a la API
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideClientHydration(withEventReplay()), provideHttpClient()]
 };
--- src/app/pages/user-view/user-view.component.ts
+++ src/app/pages/user-view/user-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ButtonsComponent } from '../../components/buttons/buttons.component';
 import { Iuser } from '../../interfaces/iuser.interface';
 import { UsersService } from '../../services/users.service';
@@ -10,6 +10,20 @@ import { UsersService } from '../../services/users.service';
   styleUrl: './user-view.component.css'
 })
 export class UserViewComponent {
+  @Input() id: string | undefined = "";
+  user: Iuser | undefined;
+  userService = inject(UsersService)
 
+  async ngOnInit() {
+    let id = String(this.id)
+    
+    try {
+      this.user = await this.userService.getbyID(this.id!)
+      console.log(this.user)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 }
